237a1cd87805cccb88855b8fc26cd2bb3a8276d8
Olga Yudkin <cvtqx@protonmail.com>
2023-03-01 10:40:06 -0500

    initial commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
